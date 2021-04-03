a1da2e30b31327634ee0f7792199d2b38338b5d4
zouhair94 <z.ridaoui94@outlook.com>
2021-04-03 16:56:17 +0300

    configure mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       debug: true,
       playground: true,
-    });
+    }),
+    MongooseModule.forRoot('mongodb://localhost/delivery')
   ],
   controllers: [AppController],
   providers: [AppService],
