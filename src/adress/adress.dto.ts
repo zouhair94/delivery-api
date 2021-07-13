ecafcf3a57f480adb752ef9d9b0aed39492c62ee
zouhair94 <z.ridaoui94@outlook.com>
2021-07-14 01:57:47 +0300

    fix dto

--- src/adress/adress.dto.ts
+++ src/adress/adress.dto.ts
@@ -2,14 +2,14 @@ import { Field, InputType, OmitType, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class inputAddress {
-    @Field()
-    title: string;
-    @Field()
-    address: string;
-    @Field({nullable: true})
-    zipCode?: number;
-    @Field({nullable: true})
-    lat?: number;
-    @Field({nullable: true})
-    lng?: number;
+  @Field()
+  title: string;
+  @Field()
+  address: string;
+  @Field({ nullable: true })
+  zipCode?: number;
+  @Field({ nullable: true })
+  lat?: number;
+  @Field({ nullable: true })
+  lng?: number;
 }
